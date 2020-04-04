06242e58f668f6fe5cd909e6ea512647f5cb8920
Gabriel Salomon <55660025+shazam2064@users.noreply.github.com>
2020-04-04 07:57:19 -0400

    Added all the files

---
+++ 34-drop if exists.sql
@@ -0,0 +1,18 @@
+-- Change Delimiter
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS GetLanguage//
+
+-- Create Stored Procedure
+CREATE PROCEDURE GetLanguage()
+BEGIN
+	SELECT * FROM language;
+END//
+-- Change Delimiter again
+DELIMITER ;
+
+-- Call Stored Procedure
+CALL GetLanguage();
+
+DROP PROCEDURE GetLanguage;
+
